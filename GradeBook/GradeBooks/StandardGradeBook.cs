a48683ca699a1cce42ebc24acf0123c086c74e26
diegohigueros <18dhigueros@gmail.com>
2020-11-06 08:29:11 -0700

    Created new gradebook types: StandardGradeBook and RankedGradeBook.

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,18 @@
+﻿using GradeBook.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    public class StandardGradeBook : BaseGradeBook
+    {
+        // This constructor calls the base constructor. 
+        StandardGradeBook(string name) : base(name) 
+        {
+            Type = GradeBookType.Standard;
+        }
+
+
+    }
+}
